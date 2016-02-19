c657da5c94f30da45bdfb72121c28176dc3bb5bd
ao222qg <ao222qg@student.lnu.se>
2016-02-19 17:04:39 +0100

    Added files via upload
    Final commit.

---
+++ 1.2-Rita-med-asterisker/ConsoleApplication1/1.2.Rita.med.asterisker.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // for-satsen för att bestämma antalet rader.
+            for (int row = 0; row < 25; row++)
+            {
+                
+                // Switch-satsen med hjälp av modelus bestämmer vilken rad som ska ha vilken färg.
+                    
+                    
+                // If skapar ett tomrum i början av varje rad.
+                    if (row % 2 == 1)
+                    {
+                        Console.Write(" ");
+
+                    }
+                // For skriver ut en asterisk i följd av ett tomrum på varje kolumn.
+                    for (int col = 0; col < 39; col++) 
+                    {
+                        switch (row % 3)
+                        {
+                            case 0:
+                                Console.ForegroundColor = ConsoleColor.Yellow;
+                                break;
+                            case 1:
+                                Console.ForegroundColor = ConsoleColor.Magenta;
+                                break;
+                            case 2:
+                                Console.ForegroundColor = ConsoleColor.Green;
+                                break;
+                        }
+                        Console.Write("* ");
+                    }
+                    Console.WriteLine();
+            }
+           
+            Console.ResetColor();
+        }
+    }
+}
---
+++ 1.2-Rita-med-asterisker/ConsoleApplication1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // for-satsen för att bestämma antalet rader som lopen ska skapa.
+            for (int row = 0; row < 25; row++)
+            {
+                
+                // Switch-satsen med hjälp av modelus bestämmer vilken rad som ska ha vilken färg.
+                    switch (row % 3)
+                    {
+                        case 0:
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            break;
+                        case 1:
+                            Console.ForegroundColor = ConsoleColor.Magenta;
+                            break;
+                        case 2:
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            break;
+                    }
+                // Här så placerar if ut en asterisk varannan column.
+                    if (row % 2 == 1)
+                    {
+                        Console.Write(" ");
+
+                    }
+
+                    for (int col = 0; col < 39; col++)
+                    {
+                        Console.Write("* ");
+                    }
+                    Console.WriteLine();
+            }
+           
+            Console.ResetColor();
+        }
+    }
+}
